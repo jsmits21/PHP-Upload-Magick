6caee0dd7234c754d6b516db323f778c310e90dd
Jacob Smits <jsmits21@lavabit.com>
2011-08-04 15:32:33 -0500

    Nothing new, just removed my domain name from the script.

--- website/upload/image_upload_exec.php
+++ website/upload/image_upload_exec.php
@@ -10,18 +10,18 @@ $file->theFile = $_FILES['upload']['name'];
 $file->tempFile = $_FILES['upload']['tmp_name'];
 $file->uploadName = 'upload';
 $file->maxSize = 1572864;
-$file->renameFile = true;
 $file->extensions = array(".jpg", ".png", ".gif", ".bmp");
 $file->newName = "image";
 $file->fileNum = 1;
-$file->uploadDir = "$DOCUMENT_ROOT/../thehoppr.com/testing/results/";
+$file->uploadDir = "$DOCUMENT_ROOT/../mywebsite.com/testing/results/";
 $file->conFormat = ".jpg"; //Even know this is an image manipulation function, it needs to reside in the file upload vars so the renaming function works.
 
 //Image Manipulation Vars
 
+$file->thumbDir = "$DOCUMENT_ROOT/../mywebsite.com/testing/results/thumb/";
 $file->constrain = true;
-$file->thumbDir = "$DOCUMENT_ROOT/../thehoppr.com/testing/results/thumb/";
 $file->sharpen = true;
+$file->enlarge = true;
 $file->max_X = 800;
 $file->max_Y = 600;
 $file->min_X = 300;
